99ebdedabf512df7b7254c9674834d3c40173263
Pat Lillis <lillispm@gmail.com>
2016-04-01 13:43:23 -0400

    little cleanup

--- src/acronym.js
+++ src/acronym.js
@@ -1,34 +1,58 @@
-import fs from 'fs'
-
 // returns the path to the word list which is separated by `\n`
 import wordListPath from 'word-list'
+import fs from 'fs'
 
-const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
 
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'
-var dictionary = {}
+// Dictionary of words
+// This is indexed by first letter.
+// dictionary.a = [ 'apricots', 'always', ...]
+const dictionary = {}
 
-for (let i = 0; i < alphabet.length; i++) {
-	dictionary[alphabet.charAt(i)] = []
+
+// Helper Functions
+const isString = (text) => typeof text === 'string'
+const isArray = (text) => text instanceof Array
+const getWord = (letter) => dictionary[letter][Math.floor(Math.random() * dictionary[letter].length)]
+
+
+// Set up the dictionary of words using word-list
+function initialize () {
+	const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+	// Empty array for each letter
+	for (let i = 0; i < alphabet.length; i++) {
+		dictionary[alphabet.charAt(i)] = []
+	}
+
+	// Put all the words into the right spot.
+	for (let i = 0; i < wordArray.length; i++) {
+		dictionary[wordArray[i].charAt(0)].push(wordArray[i])
+	}
 }
 
-for (let i = 0; i < wordArray.length; i++) {
-	dictionary[wordArray[i].charAt(0)].push(wordArray[i])
-}
 
-function acronym (text) {
+// Main function
+function acronym (text, {
+	capitalization 	= true,
+	separator		= ' ',
+	minWordLength	= 1,
+	maxWordLength	= undefined
+} = { }) {
 	if (isString(text)) {
+		// Return acronym for this word
 		return getWord(text.charAt(0))
 	} else if (isArray(text)) {
+		// Return acronym for each word in the array.
 		return text
 	} else {
+		// ???
 		return undefined
 	}
 }
 
 
-const isString = (text) => typeof text === 'string'
-const isArray = (text) => text instanceof Array
-const getWord = (letter) => dictionary[letter][Math.floor(Math.random() * dictionary[letter].length)]
 
+// Exports
+initialize()
 export default acronym
